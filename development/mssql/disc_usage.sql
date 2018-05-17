88c955d603d93e599005f6e13d15bd7635a702be
Radosław Osiński <Radoslaw.Osinski@cwsfe.pl>
2018-05-17 10:24:58 +0200

    Create disc_usage.sql

---
+++ development/mssql/disc_usage.sql
@@ -0,0 +1,10 @@
+SELECT
+  (SELECT CONVERT(DECIMAL(18,2), SUM(CAST(df.size as float))*8/1024.0)
+   FROM sys.database_files AS df
+   WHERE df.type in ( 0, 2, 4 ) ) AS [DbSize],
+  CONVERT(DECIMAL(18,2), SUM(a.total_pages)*8/1024.0) AS [SpaceUsed],
+  (SELECT CONVERT(DECIMAL(18,2), SUM(CAST(df.size as float))*8/1024.0)
+   FROM sys.database_files AS df
+   WHERE df.type in (1, 3)) AS [LogSize]
+FROM sys.partitions p join sys.allocation_units a
+    on p.partition_id = a.container_id;
