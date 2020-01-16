d1d9b28e6e4ec1d3b44ae72079dee4c10bac6736
Radosław Osiński <Radoslaw.Osinski@gmail.com>
2020-01-16 15:35:07 +0100

    Update method_body.sql

--- development/mssql/method_body.sql
+++ development/mssql/method_body.sql
@@ -1,1 +1,9 @@
-
+SELECT DISTINCT 
+	   o.name AS Object_Name,
+       o.type_desc,
+	   m.definition
+  FROM sys.sql_modules m
+       INNER JOIN
+       sys.objects o
+         ON m.object_id = o.object_id
+ WHERE o.name Like '%name%';
